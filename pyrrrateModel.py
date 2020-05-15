6a7c57f8f504e1ef0441b4b62fef63ad23c58c76
Hannes Baukmann <hannes.baukmann@fu-berlin.de>
2020-05-15 13:34:16 +0200

    added outline for FBA method

--- pyrrrateModel.py
+++ pyrrrateModel.py
@@ -3,6 +3,17 @@ class rrrModel:
     def print_numbers():
         pass
 
+    def FBA(self, objective=None):
+        if not objective:
+            # default objectiv is biomass function
+            # search for biomass reaction
+            # if not found -> Error
+            pass
+        else:
+            # check whether rxn exists
+            pass
+        # if vmin, vmas aren't given for a reaction, set it to -1000,1000
+
 #species
 #- metabolites
 #- intra
