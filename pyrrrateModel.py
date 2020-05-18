484a86f505499dfc74c8dfd8211cc9a54ff66f95
Hannes Baukmann <hannes.baukmann@fu-berlin.de>
2020-05-18 13:44:13 +0200

    added method to print numbers of species and reactions

--- pyrrrateModel.py
+++ pyrrrateModel.py
@@ -39,29 +39,49 @@ class Model(object):
         # Create an empty variable to save the latest results of the simulation methods
         self.results = None
 
-        def print_numbers():
-            pass
-            # species
-            # - metabolites
-            # - intra
-            # - extra
-            # - macromolecules
-            # - enzymes
-            # - RE
-            # - NRE
-            # - TF
-            # - quota
-            # - storage
-            # reactions
-            # - uptake
-            # - met
-            # - translation
-            # - spontaneous
-            # - maintenance
-            # regulation
-            # - rules
-            # - Regulatory P
-            # - regulated reactions
+        def print_numbers(model):
+            extra = 0
+            quota = 0
+            stor = 0
+
+            spon = 0
+            main = 0
+
+            for met in model.metabolites_dict.keys():
+                if model.metabolites_dict[met]['speciesType'] == 'extracellular':
+                    extra += 1
+
+            for mm in model.macromolecules_dict.keys():
+                if model.macromolecules_dict[mm]['speciesType'] == 'quota':
+                    quota += 1
+                elif model.macromolecules_dict[mm]['speciesType'] == 'storage':
+                    stor += 1
+
+            for rxn in model.reactions_dict.keys():
+                if model.reactions_dict[rxn]['maintenanceScaling'] > 0.0:
+                    main += 1
+                if not model.reactions_dict[rxn]['geneProduct']:
+                    spon += 1
+
+            print('species\t\t\t\t' + str(len(model.metabolites_dict) + len(model.macromolecules_dict)) \
+                  + '\n\t metabolites\t\t' + str(len(model.metabolites_dict)) \
+                  + '\n\t\t extracellular\t' + str(extra) \
+                  + '\n\t\t intracellular\t' + str(len(model.metabolites_dict) - extra) \
+                  + '\n\t macromolecules\t\t' + str(len(model.macromolecules_dict)) \
+                  + '\n\t\t enzymes\t' + str(len(model.macromolecules_dict) - quota - stor) \
+                  + '\n\t\t quota\t\t' + str(quota) \
+                  + '\n\t\t storage\t' + str(stor) \
+                  + '\n reactions\t\t\t' + str(len(model.reactions_dict)) \
+                  + '\n\t uptake\t\t' \
+                  + '\n\t metabolic\t\t' \
+                  + '\n\t translation\t\t' \
+                  + '\n\t spontaneous\t\t' + str(spon) \
+                  + '\n\t maintenance\t\t' + str(main) \
+                  + '\n regulation\t\t\t\t' \
+                  + '\n\t rules\t\t' \
+                  + '\n\t regulatory proteins\t\t' \
+                  + '\n\t regulated reactions\t\t')
+
 
         def FBA(self, objective=None):
             if not objective:
