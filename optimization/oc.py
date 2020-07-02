90b1f78cfc800d945880aa7a839fa6e0d15131b3
Markus <markus.koebis+git@gmail.com>
2020-07-02 16:34:00 +0200

    - code hygiene

--- optimization/oc.py
+++ optimization/oc.py
@@ -6,17 +6,17 @@ import scipy.sparse as sp
 from . import lp as lp_wrapper
 
 
-def cp_linprog(t_0, t_end, phi1, phi2, phi3, smat1, smat2, lb, ub, h, Hy, Hu,
-               By0, Byend, b_bndry, n_steps=101, varphi=0.0):
+def cp_linprog(t_0, t_end, phi1, phi2, phi3, smat1, smat2, lbvec, ubvec, hvec, hmaty, hmatu,
+               bmaty0, bmatyend, b_bndry, n_steps=101, varphi=0.0):
     """
     Approximate the solution of the optimal control problem
      min int_{t_0}^{t_end} phi1^T y dt + phi2^T y_0 + phi3^T y_end
      s.t. y' = smat2 u
           0  = smat1 u
-          lb <= u <= ub
-          Hy y + Hu u <= h
+          lbvec <= u <= ubvec
+          hmaty y + hmatu u <= hvec
           0 <= y
-          By0*y_0 + Byend*y_end = b_bndry
+          bmaty0*y_0 + bmatyend*y_end = b_bndry
     using complete parameterization with N time intervals and midpoint rule for
      DAE integration and trapezoidal rule for approximating the integral in the
      objective
@@ -28,7 +28,7 @@ def cp_linprog(t_0, t_end, phi1, phi2, phi3, smat1, smat2, lb, ub, h, Hy, Hu,
     @TODO
      - create/use a class for the time series data in the output
      - more security checks
-     - add additional terms in objectvie and dynamics
+     - add additional terms in objective and dynamics
      - allow irregular time grid
      - ...
     """
@@ -38,16 +38,16 @@ def cp_linprog(t_0, t_end, phi1, phi2, phi3, smat1, smat2, lb, ub, h, Hy, Hu,
     n_allu = n_steps*n_u
     n_bndry = len(b_bndry)
 
-    tt = np.linspace(t_0, t_end, n_steps+1)
-    del_t = tt[1]-tt[0]
-    tt_shift = (tt[1:] + tt[:-1])/2.0 # time grid for controls
+    tgrid = np.linspace(t_0, t_end, n_steps+1)
+    del_t = tgrid[1]-tgrid[0]
+    tt_shift = (tgrid[1:] + tgrid[:-1])/2.0 # time grid for controls
 
     # Discretization of objective
     # Lagrange part @MAYBE: add possib. for  more complicated objective
     f_y = np.hstack([0.5*del_t*phi1,
                      np.hstack((n_steps-1)*[del_t*phi1]),
                      0.5*del_t*phi1])
-    expvals = np.exp(-varphi*tt)
+    expvals = np.exp(-varphi*tgrid)
     f_y *= np.repeat(expvals, n_y)
     f_u = np.array(n_allu*[0.0])
     # Mayer part
@@ -55,19 +55,19 @@ def cp_linprog(t_0, t_end, phi1, phi2, phi3, smat1, smat2, lb, ub, h, Hy, Hu,
     f_y[n_steps*n_y:n_ally] += phi3
 
     # Discretization of dynamics
-    Aeq1_y = sp.kron(sp.diags((1.0, -1.0), (0, 1), (n_steps, n_steps+1)),
-                     sp.eye(n_y))
-    Aeq1_u = sp.kron(sp.eye(n_steps), del_t*smat2)
+    aeqmat1_y = sp.kron(sp.diags((1.0, -1.0), (0, 1), (n_steps, n_steps+1)),
+                        sp.eye(n_y))
+    aeqmat1_u = sp.kron(sp.eye(n_steps), del_t*smat2)
     beq1 = np.array(n_steps*n_y*[0.0])
 
     # Discretization of QSSA rows (this is simplified and only works for constant smat1)
-    Aeq2_y = sp.csr_matrix((n_steps*n_qssa, n_ally))
-    Aeq2_u = sp.kron(sp.eye(n_steps), smat1)
+    aeqmat2_y = sp.csr_matrix((n_steps*n_qssa, n_ally))
+    aeqmat2_u = sp.kron(sp.eye(n_steps), smat1)
     beq2 = np.array(n_steps*n_qssa*[0.0])
 
     # Discretization of flux bounds @MAYBE: allow time dependency here
-    lb_u = np.hstack(n_steps*[lb])
-    ub_u = np.hstack(n_steps*[ub])
+    lb_u = np.hstack(n_steps*[lbvec])
+    ub_u = np.hstack(n_steps*[ubvec])
 
     # Discretization of positivity
     lb_y = np.array(n_ally*[0.0])
@@ -75,25 +75,25 @@ def cp_linprog(t_0, t_end, phi1, phi2, phi3, smat1, smat2, lb, ub, h, Hy, Hu,
 
     # Discretization of mixed constraints
     # TODO: Allow time dependency here
-    Aineq1_y = sp.kron(sp.diags((1.0, 1.0), (0, 1),
-                                shape=(n_steps, n_steps+1)), 0.5*Hy)
-    Aineq1_u = sp.kron(sp.eye(n_steps), Hu)
-    bineq1 = np.hstack(n_steps*[h])
+    amat1_y = sp.kron(sp.diags((1.0, 1.0), (0, 1),
+                               shape=(n_steps, n_steps+1)), 0.5*hmaty)
+    amat1_u = sp.kron(sp.eye(n_steps), hmatu)
+    bineq1 = np.hstack(n_steps*[hvec])
 
     # Discretization of equality boundary constraints @MAYBE: also inequality
-    Aeq3_y = sp.hstack([By0, sp.csr_matrix((n_bndry, (n_steps-1)*n_y)), Byend])
-    Aeq3_u = sp.csr_matrix((n_bndry, n_allu))
+    aeqmat3_y = sp.hstack([bmaty0, sp.csr_matrix((n_bndry, (n_steps-1)*n_y)), bmatyend])
+    aeqmat3_u = sp.csr_matrix((n_bndry, n_allu))
     beq3 = b_bndry
 
     # Collect all data
-    f = np.hstack([f_y, f_u])
-    Aeq = sp.bmat([[Aeq1_y, Aeq1_u],
-                   [Aeq2_y, Aeq2_u],
-                   [Aeq3_y, Aeq3_u]], format='csr')
+    f_all = np.hstack([f_y, f_u])
+    aeqmat = sp.bmat([[aeqmat1_y, aeqmat1_u],
+                      [aeqmat2_y, aeqmat2_u],
+                      [aeqmat3_y, aeqmat3_u]], format='csr')
     beq = np.hstack([beq1, beq2, beq3])
     lb_all = np.hstack([lb_y, lb_u])
     ub_all = np.hstack([ub_y, ub_u])
-    Aineq = sp.bmat([[Aineq1_y, Aineq1_u]], format='csr')
+    amat = sp.bmat([[amat1_y, amat1_u]], format='csr')
     bineq = bineq1
 
     # TODO: Create variable name creator function
@@ -101,15 +101,15 @@ def cp_linprog(t_0, t_end, phi1, phi2, phi3, smat1, smat2, lb, ub, h, Hy, Hu,
     variable_names_u = ["u_"+str(j+1)+"_"+str(i) for i in range(n_steps) for j in range(n_u)]
     variable_names = variable_names_y + variable_names_u
 
-    Model = lp_wrapper.LPModel(name="OC Model - Full par., midpoint rule")
-    Model.sparse_model_setup(f, Aineq, bineq, Aeq, beq, lb_all, ub_all, variable_names)
+    model = lp_wrapper.LPModel(name="OC Model - Full par., midpoint rule")
+    model.sparse_model_setup(f_all, amat, bineq, aeqmat, beq, lb_all, ub_all, variable_names)
 
-    Model.optimize()
+    model.optimize()
 
-    if Model.status == lp_wrapper.OPTIMAL: # @FIXME: This is gurobi specific.
-        y_data = np.reshape(Model.get_solution()[:n_ally], (n_steps+1, n_y))
-        u_data = np.reshape(Model.get_solution()[n_ally:], (n_steps, n_u))
-        return tt, tt_shift, y_data, u_data
+    if model.status == lp_wrapper.OPTIMAL: # @FIXME: This is gurobi specific.
+        y_data = np.reshape(model.get_solution()[:n_ally], (n_steps+1, n_y))
+        u_data = np.reshape(model.get_solution()[n_ally:], (n_steps, n_u))
+        return tgrid, tt_shift, y_data, u_data
     else:
         # TODO: use cleverer output here
         print("No solution found")
