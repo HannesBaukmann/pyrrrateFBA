87ab72f7874740561536d3cb44afe82191ab3a20
MarkusKoebis <markus.koebis@gmail.com>
2020-11-16 14:53:11 +0100

    OC
    First version of the "dynamic Kronecker product"

--- util/linalg.py
+++ util/linalg.py
@@ -54,3 +54,159 @@ def solve_if_unique(amat, bvec):
             return None
         return lsqout[0]
     raise TypeError('Coefficient matrix must be a numpy nd array or a csr sparse matrix')
+
+
+#def svd_wrapper(amat):
+#    """
+#    singular value decomposition
+#    """
+#    # scipy.sparse.linalg.svds(A, k=6, ncv=None, tol=0, which='LM', v0=None, maxiter=None,
+#    return_singular_vectors=True, solver='arpack')[source]
+#    """
+#    u   ndarray, shape=(M, k)
+#        Unitary matrix having left singular vectors as columns. If return_singular_vectors
+#        is “vh”, this variable is not computed, and None is returned instead.
+#    s   ndarray, shape=(k,)
+#        The singular values.
+#    vt  ndarray, shape=(k, N)
+#        Unitary matrix having right singular vectors as rows. If return_singular_vectors
+#        is “u”, this variable is not computed, and None is returned instead.
+#    """
+#    #
+#    return None
+
+
+def _check_A_T_shape(A, T, along_rows):
+    m_A, n_A = A.shape
+    m_T, n_T = T.shape
+    if not m_A == m_T:
+        raise AttributeError("Number of rows of matrices A and T must be the same.")
+    if not along_rows:
+        if not n_A == n_T:
+            raise AttributeError("Number of rows in A must equal number of rows in T.")
+    #if min([m_A, n_A, m_B, n_B]) == 0:
+    #    return True
+    #return False
+
+
+def dkron(A, B, T, along_rows=False, out_type='csr'):
+    """
+    Dynamic extension of the Kronecker product
+    dkron(A, B, T) = ( a_11*B(T_11), a_12*B(T_12) ... )
+    if B is not callable, it reduces to a regular Kronecker
+    """
+    #
+    sparse_out_types = ['csr']
+    _check_A_T_shape(A, T, along_rows)
+    #
+    if callable(B):
+        #print('B is callable')
+        if out_type == 'csr':
+            C = _dkron_csr(A, B, T, along_rows)
+        else: # default (after csr) to numpy
+            C = _dkron_np(A, B, T, along_rows)
+    else:
+        #print('B is NOT callable')
+        if out_type in sparse_out_types:
+            C = sp.kron(A, B, format=out_type)
+            # This is actually slow in scipy 1.5.0 because matrices are converted internally
+        else: # default to np
+            C = np.kron(A, B)
+    #
+    return C
+
+
+def _dkron_csr(A, B, T, along_rows):
+    """
+    Dynamic Kronecker product for csr matrices
+    """
+    m_A, n_A = A.shape
+    if min([m_A, n_A]) == 0:
+        B0 = _ensure_csr(B(0.0))
+    else:
+        B0 = _ensure_csr(B(T[0, 0]))
+    m_B, n_B = B0.shape
+    if min([m_A, m_B, n_A, n_B]) == 0:
+        return sp.csr_matrix((m_A*m_B, n_A*n_B))
+    nnz_B = B0.nnz
+    # allocate
+    safety_factor = 1.2
+    n_alloc = int(safety_factor*nnz_B*m_A*m_B)+1 # FIXME: What if safety_factor is too small?
+    data = n_alloc*[0.0]
+    row_ind = n_alloc*[0]
+    col_ind = n_alloc*[0]
+    k = 0
+    in_loop = False
+    A = _ensure_csr(A)
+    #for a_entry, a_col, a_row in zip(A.data, A.indices, _csr_create_full_row_vector(A)):
+        # -> Then no double-for-loop
+    for a_row in range(m_A):
+        in_inner_loop = True
+        for a_entry, a_col in zip(A.data[A.indptr[a_row]:A.indptr[a_row+1]],
+                                  A.indices[A.indptr[a_row]:A.indptr[a_row+1]]):
+            if not along_rows and in_loop:
+                B0 = _ensure_csr(B(T[a_row, a_col]))
+            if along_rows and in_inner_loop and in_loop:
+                B0 = _ensure_csr(B(T[a_row, 0]))
+                in_inner_loop = False
+            in_loop = True
+            knew = k+len(B0.data)
+            data[k:knew] = a_entry*B0.data
+            col_ind[k:knew] = [n_B*a_col+i for i in B0.indices]
+            row_ind[k:knew] = [m_B*a_row+i for i in _csr_create_full_row_vector(B0)]
+            k = knew
+    return sp.csr_matrix((data, (row_ind, col_ind)), shape=(m_A*m_B, n_A*n_B))
+    # @MAYBE: csr_matrix((data, indices, indptr), shape=(., .)) could be a bit faster
+    # as this is the natural storage of csr
+
+
+def _csr_create_full_row_vector(mat_in):
+    """
+    row numbers from csr_matrix (like in the coo format)
+    # TODO: Das muss eleganter gehen, vllt. einen iterator ausgeben?
+    """
+    mat_out = len(mat_in.indices)*[0]
+    k = 0
+    for i, row in enumerate(mat_in):
+        knew = k + row.nnz
+        mat_out[k:knew] = (knew-k)*[i]
+        k = knew
+    return mat_out
+
+
+def _ensure_csr(mat_in):
+    """
+    If the matrix is not csr: Make it so
+    """
+    if isinstance(mat_in, sp.csr_matrix):
+        return mat_in
+    if isinstance(mat_in, np.ndarray):
+        return sp.csr_matrix(mat_in)
+    return mat_in.tocsr()
+
+
+def _dkron_np(A, B, T, along_rows):
+    """
+    Dynamic Kronecker for numpy arrays
+    """
+    m_A, n_A = A.shape
+    if min([m_A, n_A]) == 0:
+        B0 = B(0.0)
+    else:
+        B0 = B(T[0, 0])
+    m_B, n_B = B0.shape
+    C = np.zeros((m_A*m_B, n_A*n_B))
+    if min([m_A, m_B, n_A, n_B]) == 0:
+        return C
+    in_loop = False
+    for a_row in range(m_A):
+        in_inner_loop = True
+        for a_col in range(n_A):
+            if not along_rows and in_loop:
+                B0 = B(T[a_row, a_col])
+            if along_rows and in_inner_loop and in_loop:
+                B0 = B(T[a_row, 0])
+                in_inner_loop = False
+            in_loop = True
+            C[m_B*a_row:m_B*a_row+m_B, n_B*a_col:n_B*a_col+n_B] = A[a_row, a_col]*B0
+    return C
