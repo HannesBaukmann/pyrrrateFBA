dbf339983f516f6f19020aca2a9168e2e2a8b17a
MarkusKoebis <markus.koebis@gmail.com>
2020-11-24 12:45:21 +0100

    Time integration: Runge-Kutta utils
    added some explicit schemes and adjoint RK methods

--- util/runge_kutta.py
+++ util/runge_kutta.py
@@ -9,7 +9,7 @@ class RungeKuttaPars:
     """
     Collect the parameters of the Butcher tableau for several Runge-Kutta schemes
     """
-    def __init__(self, s=2, family='LobattoIIIA'):
+    def __init__(self, s=2, family='LobattoIIIA', force_set=None):
         """
         Create the RK matrix and weight vector based on the underlying (collocation) scheme.
         Numbers are hardcoded
@@ -17,66 +17,92 @@ class RungeKuttaPars:
         if not isinstance(s, int) or s < 1:
             raise ValueError('Stage number s of RK method must be a positive integer')
         # --------------------------------------------------------------------
-        # It might make sense to ask for things like FSAL, zero rows, is_collocation etc.
-        self.family = family
-        #
-        print(s)
-        if family == 'LobattoIIIA':
-            if s == 1:
-                raise ValueError('Lobatto methods must have at least two stages')
-            elif s == 2: # Trapezoidal rule
-                self.A = np.array([[0, 0],
-                                   [0.5, 0.5]])
-                self.b = np.array([[0.5, 0.5]])
-            elif s == 3:
-                self.A = np.array([[0, 0, 0],
-                                   [5/24, 1/3, -1/24],
-                                   [1/6, 2/3, 1/6]])
-                self.b = np.array([[1/6, 2/3, 1/6]])
+        if force_set is None:
+            # It might make sense to ask for things like FSAL, zero rows, is_collocation etc.
+            self.family = family
+            #
+            if family == 'LobattoIIIA':
+                if s == 1:
+                    raise ValueError('Lobatto methods must have at least two stages')
+                elif s == 2: # Trapezoidal rule
+                    self.A = np.array([[0, 0],
+                                       [0.5, 0.5]])
+                    self.b = np.array([[0.5, 0.5]])
+                elif s == 3:
+                    self.A = np.array([[0, 0, 0],
+                                       [5/24, 1/3, -1/24],
+                                       [1/6, 2/3, 1/6]])
+                    self.b = np.array([[1/6, 2/3, 1/6]])
+                else:
+                    raise ValueError('LobattoIIIA methods only implemented for s < 4')
+                    # TODO: For higher orders: Get from simplified conditions(?)
+            # --------------------------------------------------------------------
+            elif family == 'RadauIIA':
+                if s == 1: # implicit Euler
+                    self.A = np.array([[1.0]])
+                    self.b = np.array([[1.0]])
+                elif s == 2: # Radau 3
+                    self.A = np.array([[5/12, -1/12],
+                                       [3/4, 1/4]])
+                    self.b = np.array([[3/4, 1/4]])
+                elif s == 3: # RADAU5
+                    self.A = np.array([[(88-7*np.sqrt(6))/360,
+                                        (296-169*np.sqrt(6))/1800,
+                                        (-2+3*np.sqrt(6))/225],
+                                       [(296+169*np.sqrt(6))/1800,
+                                        (88+7*np.sqrt(6))/360,
+                                        (-2-3*np.sqrt(6))/225],
+                                       [(16-np.sqrt(6))/36, (16+np.sqrt(6))/36, 1/9]])
+                    self.b = np.array([[(16-np.sqrt(6))/36, (16+np.sqrt(6))/36, 1/9]])
+                else:
+                    raise ValueError('RadauIIA methods only implemented for s < 4')
+            # --------------------------------------------------------------------
+            elif family == 'Gauss':
+                if s == 1: # implicit midpoint rule
+                    self.A = np.array([[0.5]])
+                    self.b = np.array([[1.0]])
+                elif s == 2:
+                    self.A = np.array([[1/4, (3-2*np.sqrt(3))/12],
+                                       [(3+2*np.sqrt(3))/12, 1/4]])
+                    self.b = np.array([[0.5, 0.5]])
+                elif s == 3:
+                    self.A = np.array([[5/36, (10-3*np.sqrt(15))/45, (25-6*np.sqrt(15))/180],
+                                       [(10+3*np.sqrt(15))/72, 2/9, (10-3*np.sqrt(15))/72],
+                                       [(25+6*np.sqrt(15))/180, (10+3*np.sqrt(15))/45, 5/36]])
+                    self.b = np.array([[5/18, 4/9, 5/18]])
+                else:
+                    raise ValueError('Gauss methods only implemented for s < 4')
+            # --------------------------------------------------------------------
+            elif family == 'Explicit1':
+                if s == 1: # explicit Euler
+                    self.A = np.array([[0.0]])
+                    self.b = np.array([[1.0]])
+                elif s == 2: # Heun
+                    self.A = np.array([[0.0, 0.0],[1.0, 0.0]])
+                    self.b = np.array([[0.5, 0.5]])
+                elif s == 3: # Kutta/Simpson
+                    self.A = np.array([[0.0, 0.0, 0.0],
+                                       [1/2, 0.0, 0.0],
+                                       [-1.0, 2.0, 0.0]])
+                    self.b = np.array([[1/6, 2/3, 1/6]])
+                elif s == 4: # RK4
+                    self.A = np.array([[0.0, 0.0, 0.0, 0.0],
+                                       [0.5, 0.0, 0.0, 0.0],
+                                       [0.0, 0.5, 0.0, 0.0],
+                                       [0.0, 0.0, 1.0,0.0]])
+                    self.b = np.array([[1/6, 1/3, 1/3, 1/6]])
+                else:
+                    raise ValueError('Explicit methods (I) only implemented for s < 5')
             else:
-                raise ValueError('LobattoIIIA methods only implemented for s < 4')
-                # TODO: For higher orders: Get from simplified conditions(?)
-        # --------------------------------------------------------------------
-        elif family == 'RadauIIA':
-            if s == 1: # implicit Euler
-                self.A = np.array([[1.0]])
-                self.b = np.array([[1.0]])
-            elif s == 2: # Radau 3
-                self.A = np.array([[5/12, -1/12],
-                                   [3/4, 1/4]])
-                self.b = np.array([[3/4, 1/4]])
-            elif s == 3: # RADAU5
-                self.A = np.array([[(88-7*np.sqrt(6))/360,
-                                    (296-169*np.sqrt(6))/1800,
-                                    (-2+3*np.sqrt(6))/225],
-                                   [(296+169*np.sqrt(6))/1800,
-                                    (88+7*np.sqrt(6))/360,
-                                    (-2-3*np.sqrt(6))/225],
-                                   [(16-np.sqrt(6))/36, (16+np.sqrt(6))/36, 1/9]])
-                self.b = np.array([[(16-np.sqrt(6))/36, (16+np.sqrt(6))/36, 1/9]])
-            else:
-                raise ValueError('RadauIIA methods only implemented for s < 4')
-        # --------------------------------------------------------------------
-        elif family == 'Gauss':
-            if s == 1: # implicit midpoint rule
-                self.A = np.array([[0.5]])
-                self.b = np.array([[1.0]])
-            elif s == 2:
-                self.A = np.array([[1/4, (3-2*np.sqrt(3))/12],
-                                   [(3+2*np.sqrt(3))/12, 1/4]])
-                self.b = np.array([[0.5, 0.5]])
-            elif s == 3:
-                self.A = np.array([[5/36, (10-3*np.sqrt(15))/45, (25-6*np.sqrt(15))/180],
-                                   [(10+3*np.sqrt(15))/72, 2/9, (10-3*np.sqrt(15))/72],
-                                   [(25+6*np.sqrt(15))/180, (10+3*np.sqrt(15))/45, 5/36]])
-                self.b = np.array([[5/18, 4/9, 5/18]])
-            else:
-                raise ValueError('Gauss methods only implemented for s < 4')
-        else:
-            raise ValueError('Unknown Collocation type: ', family)
-        # Create vector c based on entries of matrix A (this is actually not necessarily
-        # true for all RK schemes)
-        self.update_c()
+                raise ValueError('Unknown Runge-Kutta family: ', family)
+            # Create vector c based on entries of matrix A (this is actually not necessarily
+            # true for all RK schemes)
+            self.update_c()
+        else:  # Just believe me, I know what I am doing...
+            self.family = None
+            self.A = force_set['A']
+            self.b = force_set['b']
+            self.c = force_set['c']
 
 
     def __str__(self):
@@ -91,7 +117,7 @@ class RungeKuttaPars:
 
     def update_c(self, c_in=None):
         """
-        calculate/set stage vector c 
+        calculate/set stage vector c
         """
         if c_in is None:
             self.c = np.sum(self.A, axis=1, keepdims=True) # based on knot condition
@@ -102,8 +128,24 @@ class RungeKuttaPars:
 
     #def check_simplified_conditions(self, cond_type):
     #def special_matrices # matrices from simplified conditions -> automatically generate
-    #def has_fsal(self):, def has_unnatural_coefficients(self): # check for 0 <= c_i,b_i <= 1, sum(b) = 1
+    #def has_fsal(self):, def has_unnatural_coefficients(self):
+        # check for 0 <= c_i,b_i <= 1, sum(b) = 1
     # dense output helpers?
+
+    def adjoint_method(self):
+        """
+        Create a dict with parameters of the "reversed-time method"
+        """
+        s_rk = self.get_stage_number()
+        b = np.zeros((1, s_rk))
+        c = np.zeros((s_rk, 1))
+        amat = np.zeros((s_rk, s_rk))
+        for i in range(s_rk):
+            b[0, i] = self.b[0, s_rk-i-1]
+            c[i, 0] = 1.0 - self.c[s_rk-i-1, 0]
+            for j in range(s_rk):
+                amat[i, j] = self.b[0, s_rk-j-1] - self.A[s_rk-i-1, s_rk-j-1]
+        return {'A': amat, 'b': b, 'c': c}
 
 
     def check_order_conditions(self, order=1):
@@ -141,3 +183,18 @@ class RungeKuttaPars:
              (np.dot(self.b, np.dot(self.A, np.dot(self.A, np.dot(self.A, self.c)))) - 1/120)[0, 0]
              ]
         raise ValueError('Higher order conditions not yet implemented')
+
+
+
+class SimplifiedConditions:
+    """
+    Simplified Conditions B(s), C(s), D(s) in the sense of Butcher
+    """
+    def __init__(self, rkm):
+        s = rkm.get_stage_number()
+        # C_s = (c_i^j/j)
+        self.Cs = np.hstack([rkm.c**j/j for j in range(1, s+1)])
+        self.eH = np.array([[1/j for j in range(1, s+1)]])
+        self.B = np.diag(rkm.b.flatten())
+        self.Ns = np.tile(np.array([1/j for j in range(1, s+1)]), (s, 1))
+        self.Vs = np.hstack([rkm.c**j for j in range(s)])
