21864ead2e636a70b4ea13e91a9e8f7cfa9bdac4
MarkusKoebis <markus.koebis@gmail.com>
2020-11-27 15:44:19 +0100

    Total Variation: LP handler
    First iteration of the extended LP class to handle abs constraint and/or objectives

--- optimization/lp.py
+++ optimization/lp.py
@@ -26,13 +26,13 @@ if DEFAULT_SOLVER == 'soplex':
         MINIMIZE = 'minimize'
     except ImportError:
         DEFAULT_SOLVER = 'glpk'
-        # Module constants
-        #INFINITY =
-        OPTIMAL = 'opt'
-        #MINIMIZE =
 if DEFAULT_SOLVER == 'glpk':
     try:
         import glpk
+        # Module constants
+        INFINITY = 1.0e20 # No reason whatsoever to choose this :-)
+        OPTIMAL = 'opt'
+        MINIMIZE = 'min'
     except ImportError:
         DEFAULT_SOLVER = 'scipy'
 if DEFAULT_SOLVER == 'scipy':
@@ -51,7 +51,7 @@ MINUSBIGM = -BIGM
 class LPModel():
     """
     Simple wrapper class to handle various LP solvers in a unified way
-    TODO - add more solvers, more options, allow for heuristics
+    TODO - add more solvers, more options, more output/statistics, allow for heuristics
     """
     def __init__(self, name=""):
         self.name = name
@@ -76,12 +76,16 @@ class LPModel():
                 return None
             return self.solver_model.x
         elif self.solver_name == 'soplex':
-            if self.status != 'optimal':
+            if self.status != OPTIMAL:
                 return None
             #return np.array([self.solver_model.getVal(x) for x in self.solver_model.getVars()]).transpose()
             #return np.array([self.solver_model.getVal(x) for x in self.solver_model.getVars()]) scip sorts the variables: Booleans first
             return np.array([self.solver_model.getVal(x) for x in self.solver_model.data])
             # MAYBE: find a more elegant and less error-prone way of extracting solutions here
+        elif self.solver_name == 'glpk':
+            if self.status != OPTIMAL:
+                return None
+            return np.array([c.primal for c in self.solver_model.cols])# FIXME: Geht das nur fuer LPs?
         return None
 
 
@@ -95,6 +99,9 @@ class LPModel():
                  lbvec <= x <= ubvec
             and the variable names stored in the list "variable_names"
         """
+        #
+        # TODO: Check dimensions first
+        #
         if self.solver_name == 'gurobi':
             _sparse_model_setup_gurobi(self.solver_model,
                                        fvec, amat, bvec, aeqmat, beq,
@@ -103,14 +110,25 @@ class LPModel():
             _sparse_model_setup_soplex(self.solver_model,
                                        fvec, amat, bvec, aeqmat, beq,
                                        lbvec, ubvec, variable_names)
-       # elif self.solver_name == 'glpk':
-       #     _sparse_model_setup_glpk(self.solver_model,
-       #                              fvec, amat, bvec, aeqmat, beq,
-       #                              lbvec, ubvec, variable_names)
+        elif self.solver_name == 'glpk':
+            _sparse_model_setup_glpk(self.solver_model,
+                                     fvec, amat, bvec, aeqmat, beq,
+                                     lbvec, ubvec, variable_names)
        # elif self.solver_name == 'scipy':
        #     _sparse_model_setup_scipy(self.solver_model,
        #                               fvec, amat, bvec, aeqmat, beq,
        #                               lbvec, ubvec, variable_names)
+
+
+    #def add_variable(self, variable_name, v_type='C'):
+    #    """
+    #    """
+    #    v_type = _translate_v_type(v_type, self.solver_name)
+    #    if self.solver_name == 'gurobi':
+    #        _add_variable_gurobi(self.solver_model, variable_name, v_type)
+    #    elif self.solver_name == 'soplex':
+    #
+    #def add_equality_constraint(self, aeqrow, bentry):
 
 
     def optimize(self):
@@ -119,10 +137,41 @@ class LPModel():
         """
         if self.solver_name == 'gurobi':
             self.solver_model.optimize()
-            self.status = self.solver_model.status
+            self.status = self.solver_model.status # MAYBE: It would probably be better to have one status meaning on the self-level
         elif self.solver_name == 'soplex':
             self.solver_model.optimize()
             self.status = self.solver_model.getStatus()
+        elif self.solver_name == 'glpk':
+            #print('-- Creating initial basis')
+            #self.solver_model.std_basis()
+            print('--- Using simplex')
+            #input('Continue?: ')
+            tmp = self.solver_model.simplex(msg_lev=self.solver_model.MSG_ALL,
+                                      presolve=True) # FIXME!!!!!!!!!!!!!! integer/intopt nur fuer MILPs
+            print(tmp)
+            #print('--- advancing basis')
+            #input('Continue?: ')
+            #self.solver_model.adv_basis()
+            #print('--- exact simplex (veeeeeeery slow?)')
+            #input('Continue?: ')
+            #self.solver_model.exact()
+            print('--- Try MILP')
+            #n_all = len(self.solver_model.cols)
+            #n_x = n_all - self.n_booles
+            #for i, c in enumerate(self.solver_model.cols):
+            #    if i >= n_x: # FIXME: Check this!!!!!!!!!!
+            #        c.kind = int
+            input('Continue?: ')
+            #self.solver_model.intopt()
+            #self.solver_model.integer()
+            assert(tmp is None)
+            #assert self.solver_model.simplex() is None         # Should not fail this way.
+            if self.solver_model.status != 'opt': return None  # If no relaxed sol., no exact sol.
+            assert self.solver_model.integer() is None         # Should not fail this way.
+            #if self.solver_model.status != 'opt': return None  # Count not find integer solution!
+            print(self.solver_model.kind)
+            self.solver_model.write(cpxlp='sr_rdefba.lp')
+            self.status = self.solver_model.status
 
 
 class MILPModel(LPModel):
@@ -167,6 +216,125 @@ class MILPModel(LPModel):
                 np.vstack([ubvec, np.ones((n_booles, 1))]), # ub
                 variable_names,
                 nbooles=n_booles)
+        elif self.solver_name == 'glpk':
+            self.n_booles = n_booles # Nicht schoen, aber vllt hilft's ja
+            _sparse_model_setup_glpk(
+                self.solver_model,
+                np.vstack([fvec, barf]), # f
+                sp.bmat([[amat, baramat]], format='csr'), # amat,
+                bvec, # bvec
+                sp.bmat([[aeqmat, sp.csr_matrix((m_aeqmat, n_booles))]], format='csr'), # aeqmat
+                beq, # beq
+                np.vstack([lbvec, np.zeros((n_booles, 1))]), # lbvec
+                np.vstack([ubvec, np.zeros((n_booles, 1))]), # ubvec
+                variable_names, # variable_names
+                nbooles=n_booles)
+
+
+class MinabsLPModel():
+    """
+    Solve the Minimization Problem
+    min fv1'*|mmf*x-nvf| + fv2'*x
+    s.t. amat*x <= bvec
+         aeq*x == beq
+         lbv <= x <= ubv
+         mmc1*|mmc2*x-nvc|+mmc3*x <= bvc
+    """
+    def __init__(self, name=''):
+        self.name = name
+        self.lp_model = LPModel(name+'_intern')
+        self.mmf = None
+        self.nvf = None
+        self.mmc1 = None
+        self.mmc2 = None
+        self.mmc3 = None
+        self.nvc = None
+        self.variable_names = []
+
+
+    def sparse_model_setup(self, fv1, mmf, nvf, fv2, amat, bvec, aeq, beq, lbv, ubv,
+                           mmc1, mmc2, nvc, mmc3, bvc, variable_names):
+        """
+        Translate min-abs-model into LP
+        """
+        # step 0(a): Save data for later
+        self.variable_names = variable_names
+        self.mmf = mmf
+        self.nvf = nvf
+        self.mmc1 = mmc1
+        self.nvc = nvc
+        #
+        m_c = bvc.shape[0]
+        m_1 = mmc2.shape[0]
+        m_f = nvf.shape[0]
+        m_leq = amat.shape[0]
+        # check dimensions, TODO: Externalize this
+        # TODO: Sort out rows/columns that are not necessary
+        n_x = fv2.shape[0]
+        m_eq = aeq.shape[0]
+        assert(fv1.shape == (m_f, 1))
+        assert((fv1 >= 0.0).all())
+        assert(mmf.shape == (m_f, n_x))
+        assert(nvf.shape == (m_f, 1))
+        assert(fv2.shape == (n_x, 1))
+        assert(amat.shape == (m_leq, n_x))
+        assert(bvec.shape == (m_leq, 1))
+        assert(aeq.shape == (m_eq, n_x))
+        assert(beq.shape == (m_eq, 1))
+        assert(lbv.shape == (n_x, 1))
+        assert(ubv.shape == (n_x, 1))
+        assert(mmc1.shape == (m_c, m_1))
+        assert(all(mmc1.data >= 0.0))
+        assert(mmc2.shape == (m_1, n_x))
+        assert(nvc.shape == (m_1, 1))
+        assert(mmc3.shape == (m_c, n_x))
+        assert(bvc.shape == (m_c, 1))
+        #
+        i_mf = sp.eye(m_f, format='csr')
+        i_m1 = sp.eye(m_1, format='csr')
+        LARGE_NUMBER = INFINITY
+        internal_v_names = variable_names + ['MinAbs__f_'+str(i) for i in range(m_f)] \
+                                          + ['MinAbs__c_'+str(i) for i in range(m_1)]
+        self.lp_model.sparse_model_setup(np.vstack([fv2, fv1, np.zeros((m_c, 1))]),  # fvec
+                     sp.bmat([[amat,  None,  None],
+                              [mmf,   -i_mf, None],
+                              [-mmf,  -i_mf, None],
+                              [mmc2,  None,  -i_m1],
+                              [-mmc2, None,  -i_m1],
+                              [mmc3,  None,  mmc1]], format='csr'),                 # amat
+                     np.vstack([bvec, nvf, -nvf, nvc, -nvc, bvc]),                  # bvec
+                     sp.bmat([[aeq, sp.csr_matrix((m_eq, m_c+m_f))]],format='csr'), # aeqmat
+                     beq,                                                           # beq
+                     np.vstack([lbv, np.zeros((m_f+m_c, 1))]),                      # lbvec
+                     np.vstack([ubv, LARGE_NUMBER*np.ones((m_f+m_c, 1))]),          # ubvec
+                     internal_v_names)                                            # variable_names
+
+    def get_solution(self):
+        """
+        Retrieve solution of the underlying LP
+        """
+        lp_sol = self.lp_model.get_solution()
+        n_x = len(self.variable_names)
+        #_translate_lp_min_abs(...., direction='back')
+        return lp_sol[:n_x]
+
+
+    def optimize(self):
+        """
+        Solve the underlying LP
+        """
+        self.lp_model.optimize()
+
+
+#def _translate_lp_min_abs(...., direction='forward'):
+    # step 1: Create new variables for the rows of mmf*x and mmc*x
+    # @MAYBE: One could "unique" over the rows first?
+    #idx_to_add = _unique_row_idxs_csr()
+    # step 2: Create abs-variables
+    # step 3: insert into LP
+
+#def _unique_row_idxs_csr():
+   #https://stackoverflow.com/questions/46126840/get-unique-rows-from-a-scipy-sparse-matrix
 
 
 # GUROBI - specifics ##########################################################
@@ -182,8 +350,7 @@ def _sparse_model_setup_gurobi(model, fvec, amat, bvec, aeqmat, beq, lbvec,
           A, Aeq are scipy.sparse.csr_matrix instances,
           the vector x contains nBooles binary variables "at the end"
     TODO:
-     - Include possibility for binary/integer variables/ coupling constraints
-        between binary <-> cont.
+     - Include possibility for coupling constraints between binary <-> cont.
      - More security checks: Are the system matrices really csr with sorted
         indices and are the dimensions correct in the first place?
     """
@@ -287,6 +454,59 @@ def _add_sparse_constraints_soplex(model, amat, bvec, x_variables, sense):
 
 
 # GLPK specifics ##############################################################
+def _sparse_model_setup_glpk(model, fvec, amat, bvec, aeqmat, beq, lbvec,
+                               ubvec, variable_names, nbooles=0):
+    """
+    We set up the following LP for (raw C-API) glpk:
+      min f'*x
+      s.t. A*x <= b
+           Aeq*x == beq
+           lb <= x <= ub
+    where f, b, beq, lb and ub are 1d numpy-arrays and
+          A, Aeq are scipy.sparse.csr_matrix instances,
+          the vector x contains nBooles binary variables "at the end"
+    TODO:
+     - Include possibility for binary/integer variables/ coupling constraints
+        between binary <-> cont.
+     - More security checks: Are the system matrices really csr with sorted
+        indices and are the dimensions correct in the first place?
+    """
+    # HIDE OUTPUT: model.MSG_OFF, glpk.env.term_on = False
+    if MINIMIZE != 'min':
+        model.obj.maximize = True
+        #fvec = -fvec
+    n_x = lbvec.size - nbooles
+    model.cols.add(n_x+nbooles)
+    for i, c in enumerate(model.cols):
+        c.name = variable_names[i]
+        c.bounds = lbvec[i], ubvec[i]
+        if i < n_x: # FIXME: Check this!!!!!!!!!!
+            c.kind = float
+        else:
+            c.kind = bool
+    model.obj[:] = list(fvec)
+    m_amat = amat.shape[0]
+    m_aeqmat = aeqmat.shape[0]
+    _add_sparse_constraints_glpk(model,
+                                 sp.vstack([amat, aeqmat], format='csr'),
+                                 np.vstack([bvec, beq]),
+                                 m_amat*['LEQ']+m_aeqmat*['EQ'])
+
+
+def _add_sparse_constraints_glpk(model, amat, bvec, senses):
+    """
+    bulk-add constraints of the form A*x <list of senses> b to a glpk model
+    """
+    nrows = bvec.size
+    model.rows.add(nrows)
+    for i, r in enumerate(model.rows):
+        if senses[i] == 'LEQ':
+            r.bounds = None, bvec[i]
+        elif senses[i] == 'EQ':
+            r.bounds = bvec[i], bvec[i]
+    a_coo = sp.csr_matrix.tocoo(amat)
+    tmp = list(zip(*[a_coo.row.tolist(), a_coo.col.tolist(), a_coo.data]))
+    model.matrix = tmp
 
 
 # SCIPY specifics #############################################################
