9963c8dc1fa503f877bd9e4a9695cdc9c2bb9754
MarkusKoebis <markus.koebis@gmail.com>
2021-04-25 22:25:14 +0200

    EXAMPLES
    Adapt old examples to new(er) solver structures

--- examples/WOB_example_simple_call.py
+++ examples/WOB_example_simple_call.py
@@ -6,60 +6,65 @@ Test the OC function cp_linprog using the model from
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.sparse as sp
-from ..optimization.oc import cp_linprog
-from ..optimization import lp as lp_wrapper
+from ..matrrrices import Matrrrices
+from ..util.runge_kutta import RungeKuttaPars
+from ..optimization.oc import cp_rk_linprog
+#from ..optimization import lp as lp_wrapper
 
-def build_WOB_example():
+def build_example():
     """
     Create simple model data for the small reaction chain metabolic network
+    Y -P-> M -P-> P
     """
     alpha = 2.0
-    Kkat = 1.0
-    P0 = 0.01
+    k_kat = 1.0
+    protein_0 = 0.01
     degredation_rate = 0.05
 
-    Phi1 = np.array([0.0, -1.0])
-    Phi2 = np.array([0.0, 0.0])
-    Phi3 = np.array([0.0, 0.0])
+    y_vec = {'Y', 'P'}
+    u_vec = {'V_y', 'eV_p'}
+    phi1 = np.array([[0.0],
+                     [-1.0]])
 
-    S1 = sp.csr_matrix(np.array([[1.0, -alpha]]))
+    smat1 = sp.csr_matrix(np.array([[1.0, -alpha]]))
+    smat2 = sp.csr_matrix(np.array([[-1.0, 0.0],
+                                    [0.0, 1.0]]))
+    smat4 = sp.csr_matrix(-degredation_rate*np.eye(2))
+    lbvec = np.array([[0.0], [0.0]])
+    #ubvec = np.array(2*[lp_wrapper.INFINITY])
 
-    S2 = sp.csr_matrix(np.array([[-1.0, 0.0], [0.0, 1.0]]))
+    #h_vec = np.array([0.0])
+    matrix_y = sp.csr_matrix(np.array([[0.0, -1/k_kat]]))
+    matrix_u = sp.csr_matrix(np.array([[1.0, 0.0]]))
 
-    S3 = sp.csr_matrix(np.array([[0.0, 0.0]]))
+    matrix_start = sp.csr_matrix(np.eye(2, dtype=float))
+    #matrix_end = sp.csr_matrix(np.zeros((2, 2), dtype=float))
+    vec_bndry = np.array([[5.0], [protein_0]])
 
-    S4 = sp.csr_matrix(-degredation_rate*np.eye(2))
-    lb = np.array([0.0, 0.0])
-    ub = np.array(2*[lp_wrapper.INFINITY])
-
-    h = np.array([0.0])
-    Hy = sp.csr_matrix(np.array([[0.0, -1/Kkat]]))
-    Hu = sp.csr_matrix(np.array([[1.0, 0.0]]))
-
-    By0 = sp.csr_matrix(np.eye(2, dtype=float))
-    Byend = sp.csr_matrix(np.zeros((2, 2), dtype=float))
-    b_bndry = np.array([5.0, P0])
-
-    return Phi1, Phi2, Phi3, S1, S2, S3, S4, lb, ub, h, Hy, Hu, By0, Byend, b_bndry
+    return {'y_vec': y_vec, 'u_vec': u_vec, 'x_vec': [],
+            'phi1': phi1,
+            'smat1': smat1, 'smat2': smat2, 'smat4': smat4,
+            'lbvec': lbvec,
+            'matrix_y': matrix_y, 'matrix_u': matrix_u,
+            'matrix_start': matrix_start, 'vec_bndry': vec_bndry}
 
 
-def run_WOB_example():
+def run_example():
     """
     Example call
     """
-    Phi1, Phi2, Phi3, S1, S2, S3, S4, lb, ub, h, Hy, Hu, By0, Byend, b_bndry = build_WOB_example()
-
+    mats = Matrrrices(None, **build_example())
     t_0 = 0.0
-    t_end = 10.0
-    N = 201
+    t_end = 15.0
+    n_steps = 201
 
-    tt, tt_shift, sol_y, sol_u = cp_linprog(t_0, t_end, Phi1, Phi2, Phi3, S1,
-                                            S2, S3, S4, lb, ub, h, Hy, Hu, By0,
-                                            Byend, b_bndry, n_steps=N,
-                                            varphi=0.01)
-
+    rkm = RungeKuttaPars(s=2, family='LobattoIIIA')
+    tgrid, tt_shift, sol_y, sol_u = cp_rk_linprog(mats, rkm, t_0, t_end, n_steps=n_steps,
+                                                  varphi=0.01)
     plt.subplot(2, 1, 1)
-    plt.plot(tt, sol_y)
+    plt.plot(tgrid, sol_y)
+    plt.legend(mats.y_vec)
     plt.subplot(2, 1, 2)
     plt.plot(tt_shift, sol_u)
+    plt.legend(mats.u_vec)
     plt.show()
